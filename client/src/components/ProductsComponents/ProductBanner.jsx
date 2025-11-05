0449bafdf0bfad2fe35e4c4179071bfea8b6716e
Nacho Villarraza <nachovillarraza@hotmail.com>
2025-11-05 11:17:36 -0300

    Refactorizando la estructura del código para mejorar la legibilidad y el mantenimiento.
    Agregando imágenes aleatorias a la ventana emergente de "en construcción".

--- client/src/components/ProductsComponents/ProductBanner.jsx
+++ client/src/components/ProductsComponents/ProductBanner.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './ProductBanner.css';
-import bannerImage from '../../assets/img/coffeebanner.png';
+import bannerImage from '../../assets/img/CoffeeBeans.jpeg';
 
 
 export const ProductBanner = () => {
